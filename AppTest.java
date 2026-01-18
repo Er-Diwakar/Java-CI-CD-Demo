b53c62e3c5197758ed76035f41ad6c02d201cee8
Er-Diwakar <sdiwakar943@gmail.com>
2026-01-18 14:39:38 +0530

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testApplicationRuns() {
+        assertTrue(true);
+    }
+}
